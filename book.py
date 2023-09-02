e2b652c8f6d2b40738c98340372ed8b6ff28470f
Ryan Soeyadi <rsoeyadi@Ryans-MBP.lan>
2023-09-02 10:40:12 -0400

    moved room config stuff

--- book.py
+++ book.py
@@ -16,23 +16,12 @@ def get_time_till(hour, min):
         time_remaining = (target_time - now).total_seconds()
     return time_remaining
 
-class Room:
-    def __init__(self, room_number_xpath, hour, min, am_pm, hour_xpath, min_xpath, pm_xpath):
-        self.room_number_xpath = room_number_xpath
-        self.hour = hour
-        self.min = min
-        self.am_pm = am_pm
-        self.hour_xpath = hour_xpath
-        self.min_xpath = min_xpath
-        self.pm_xpath = pm_xpath
-rooms = [Room(**data) for data in room_config.room_data] # unpack
-
 chrome_options = Options()
 # chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(config.link)
 
-for room in rooms:
+for room in room_config.rooms:
     driver.find_element("xpath", room.room_number_xpath).click() 
 
 # time_remaining = get_time_till(1, 23) # format is ([ 1 - 9 ], [ 10 - 59 ])
@@ -51,7 +40,7 @@ else:
     siblings[3].click()
 driver.find_element(By.XPATH, "/html/body/form/button[2]").click() # next button
 
-for i, room in enumerate(rooms):
+for i, room in enumerate(room_config.rooms):
     Select(driver.find_element(By.XPATH, room.hour_xpath)).select_by_value(room.hour)
     Select(driver.find_element(By.XPATH, room.min_xpath)).select_by_value(room.min)
     Select(driver.find_element(By.XPATH, room.pm_xpath)).select_by_index(room.am_pm)
@@ -65,5 +54,5 @@ driver.find_element(By.XPATH, "/html/body/form/div[4]/table/tbody/tr[3]/td[2]/in
 submit_button = driver.find_element(By.XPATH, "/html/body/form/div[4]/button")
 submit_button.click()
 
-while (True):
+while (True): # keep the browser open
     pass
